46c51c315a77a252d5ab5088f2975b3929c25758
migtorres <migtorres@gmail.com>
2017-05-04 02:22:19 +0100

    implements creation of certificate card with external data

--- public/app/assets/javascripts/controllers/credentialIndexController.js
+++ public/app/assets/javascripts/controllers/credentialIndexController.js
@@ -1,8 +1,41 @@
-angular.module('Credentials').controller('CredentialsIndexController', function($scope){
-	var credentialList = [
-								{"id":10023910 ,"courseName": "Existing Credential Course Name",
-								 "IssuedOn": "Mar 20 2017", "verified": true,
-								 "url": "http://www.credential.net/1000001"}
-							  ]
-  $scope.credentials = credentialList;
+angular.module('Credentials').controller('CredentialsIndexController', function($scope, $http){
+  $scope.credentials = [];
+
+  $scope.createCredential = function(url){
+  	magentoData(url.magento)
+  	$scope.magentoData['url'] = url.magento;
+  	$scope.credentials.push($scope.magentoData)
+  };
+
+  function magentoData(magentoUrl){
+  	lookupUrl = 'http://localhost:3000/get-url?url='
+  	$http.get( lookupUrl + magentoUrl).then(successCallback, errorCallback);
+  }
+
+	function successCallback(response){
+  	var magentoHtml = response.data.html;
+  	var handler = new Tautologistics.NodeHtmlParser.DefaultHandler(function (error, magentoHtml) {
+  		if (error)
+      	return false
+  		else
+      	return true
+			});
+  	var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
+		parser.parseComplete(magentoHtml);
+
+		var domBaseLocation = handler.dom[2].children[1].children[19].children[3].children[1].children[7].children[5]
+		$scope.magentoData = { 'courseName': domLastPath(domBaseLocation.children[1]),
+						'courseId': domLastPath(domBaseLocation.children[3]),
+						'issuedOn': domLastPath(domBaseLocation.children[5]),
+					  'status':'Verified'}
+	}
+
+	function errorCallback(error){
+  	//error code
+   	return false;
+	}
+
+	function domLastPath(selectedPath){
+		return selectedPath.children[3].children["0"].data;
+	}
 });
--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,7 @@
 // spec.js
 describe('credentials', function() {
 
+
   beforeEach(function() {
     browser.get('http://localhost:3000/');
   });
@@ -9,11 +10,37 @@ describe('credentials', function() {
     expect(browser.getTitle()).toEqual('Credential List');
   });
 
-  it('should have a preset card', function() {
-    browser.get('http://localhost:3000/');
+  
+  //var text = $('.search-bar');
+	//var valid = element(by.binding('credentialUrl.input.$valid'));
+	//var input = element(by.model('credential.magento'));
+	
+	//it('should be invalid if empty', function() {
+	//  input.clear();
+	//  input.sendKeys('');
+	//
+	//  expect(text.getText()).toEqual('');
+	//  expect(valid.getText()).toContain('false');
+	//});
+	//
+	//it('should be invalid if not url', function() {
+	//  input.clear();
+	//  input.sendKeys('box');
+	//
+	//  expect(valid.getText()).toContain('false');
+	//});
 
-    var card = $('.card');
+  it('should add a card after giving valid url', function() {
 
-    expect(card.isDisplayed()).toBe(true)
+
+    var searchBar = $('.search-bar');
+
+    searchBar.sendKeys('https://u.magento.com/certification/directory/dev/214');
+
+    var cards = $('.cards ng-scope');
+
+		element(by.css('ng-binding')).getText().then(function(text){expect(text).toContain("Developer")});
+		element(by.binding('credential.courseName')).getText().then(function(text){expect(text).toContain("yf9dus5130")});
+		element(by.binding('credential.issuedOn')).getText().then(function(text){expect(text).toContain("07/11/2014")});
   });
 });
