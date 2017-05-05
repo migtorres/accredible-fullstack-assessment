a5b0e003a57d2df70f15e9ecda17e762bbec996b
migtorres <migtorres@gmail.com>
2017-05-05 13:03:51 +0100

    rerfactors credentialindexcontroler.js

--- public/app/assets/javascripts/controllers/credentialIndexController.js
+++ public/app/assets/javascripts/controllers/credentialIndexController.js
@@ -5,6 +5,7 @@ angular.module('Credentials').controller('CredentialsIndexController', function(
   $scope.credentials = [];
   $scope.isSubmitting = false;
 
+  // Reads the form and queries magento url
   $scope.createCredential = function(url){
     $scope.error = false
     $scope.isSubmitting = true;
@@ -12,47 +13,75 @@ angular.module('Credentials').controller('CredentialsIndexController', function(
     lookUpUrl();
   };
 
+  //reads from URL
   function lookUpUrl(){
     var localUrl = 'http://localhost:3000/get-url?url=';
     $http.get( localUrl + $scope.magentoUrl).then(successCallback, errorCallback);
     $scope.httpReady = false
   }
 
+  // Does all the stuff when has success
   function successCallback(response){
     $scope.httpReady = true
     var magentoHtml = response.data.html;
-    var handler = new Tautologistics.NodeHtmlParser.DefaultHandler(function (error, magentoHtml) {
-      if (error)
-        $scope.error = true;
-      else
-        return magentoHtml;
-      });
-    var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
-    parser.parseComplete(magentoHtml);
-
-    var domBaseLocation = handler.dom[2].children[1].children[19].children[3].children[1].children[7].children[5];
-    var courseName = domLastPath(domBaseLocation.children[1]).trim()
-    $scope.magentoData = { 'courseName': courseName,
-            'courseId': domLastPath(domBaseLocation.children[3]).trim(),
-            'issuedOn': domLastPath(domBaseLocation.children[5]).trim(),
-            'status':'Verified',
-            'image': courseName + '.png',
-            'url': $scope.magentoUrl};
+    var parsedHtml = parseHtml(magentoHtml);
+    magentoData(parsedHtml);
     appendCredential();
   }
+
+  //The following functions are to parse HTML from Magento
+
+  // Uses HTML parser to get object with the dom
+  function parseHtml(magentoHtml){
+    handler = new Tautologistics.NodeHtmlParser.DefaultHandler(function (error) {
+      if (error) $scope.error = true;
+    });
+    var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
+    parser.parseComplete(magentoHtml);
+    return handler
+  };
+
+  // extracts wanted fields from DOM and sets them to $scope.magentoData
+  function magentoData(parsedHtml){
+    baseLocation = domBaseLocation(parsedHtml)
+    domFields(baseLocation)
+    jQuery.extend($scope.magentoData, { status:'Verified',
+                                        image: $scope.magentoData.courseName + '.png',
+                                        url: $scope.magentoUrl});
+  };
+
+  // Finds table where information needed sits
+  function domBaseLocation(parsedHtml){
+    try {
+      return parsedHtml.dom[2].children[1].children[19].
+             children[3].children[1].children[7].children[5];
+    } catch (err){
+      errorCallback()
+    }
+  };
+
+  // For each field the way to get it is similar and this function gets it
+  function domLastPath(selectedPath){
+    return selectedPath.children[3].children["0"].data;
+  };
+
+  // Adds directly extracted fields to magentoData $scope object
+  function domFields(domBaseLocation){
+    $scope.magentoData = { courseName: domLastPath(domBaseLocation.children[1]).trim(),
+                           courseId: domLastPath(domBaseLocation.children[3]).trim(),
+                           issuedOn: domLastPath(domBaseLocation.children[5]).trim() }
+  };
+
+  // End of HTML parsing functions
 
   function errorCallback(){
     $scope.httpReady = true
     $scope.error = true;
     $scope.isSubmitting = false;
-  }
+  };
 
   function appendCredential(){
     $scope.credentials.push($scope.magentoData);
     $scope.isSubmitting = false;
-  }
-
-  function domLastPath(selectedPath){
-    return selectedPath.children[3].children["0"].data;
-  }
+  };
 });
